f7cceda37ec657a9cba2f605efd7d5de9b1836bf
Brianee445 <edewesley13@gmail.com>
2026-04-19 16:45:48 +0100

    Delete Color.kt

--- Color.kt
+++
@@ -1,8 +0,0 @@
-package com.kate.assistant.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val KateCyan    = Color(0xFF00E5FF)
-val KateDark    = Color(0xFF060B10)
-val KateSurface = Color(0xFF0D1B2A)
-val KateText    = Color(0xFFE0F7FA)
