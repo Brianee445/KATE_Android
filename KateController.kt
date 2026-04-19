05eb35ada140d0a4ec206865f44b99d98b7497c1
Brianee445 <edewesley13@gmail.com>
2026-04-19 18:11:46 +0100

    Delete KateController.kt

--- KateController.kt
+++
@@ -1,7 +0,0 @@
-package com.kate.assistant.features.device
-
-// Convenience facade combining hardware + device control
-class KateController(context: android.content.Context) {
-    val hardware = KateHardwareController(context)
-    val device   = KateDeviceController(context)
-}
