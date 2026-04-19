a0b08fea606433f58ca24f310710178ebfb4dde7
Brianee445 <edewesley13@gmail.com>
2026-04-19 16:20:56 +0100

    Add files via upload

---
+++ app/src/main/kotlin/com/kate/assistant/ui/Color.kt
@@ -0,0 +1,8 @@
+package com.kate.assistant.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val KateCyan    = Color(0xFF00E5FF)
+val KateDark    = Color(0xFF060B10)
+val KateSurface = Color(0xFF0D1B2A)
+val KateText    = Color(0xFFE0F7FA)
