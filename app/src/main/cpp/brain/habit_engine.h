b62e5599bc39eddf252a6c089ab832bd2c21be20
Brianee445 <edewesley13@gmail.com>
2026-04-24 14:07:30 +0100

    Add load_habit function to habit_engine.h

--- app/src/main/cpp/brain/habit_engine.h
+++ app/src/main/cpp/brain/habit_engine.h
@@ -3,6 +3,7 @@
 
 void record_habit(const char* intent, const char* entity);
 const char* get_preferred_entity(const char* intent);
+const char* get_suggestion(int hour);
 
 // NEW
 void load_habit(const char* intent, const char* entity, int count);
