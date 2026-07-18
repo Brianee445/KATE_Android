837fe6299fdb564596f49e5fdd39b0b9d0aabe91
Brianee445 <edewesley13@gmail.com>
2026-07-18 12:26:10 +0100

    Update KateAvatar.kt

--- app/src/main/java/com/dti/kate/ui/components/KateAvatar.kt
+++ app/src/main/java/com/dti/kate/ui/components/KateAvatar.kt
@@ -1,3 +1,21 @@
+package com.dti.kate.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.dti.kate.R
+import com.dti.kate.ui.theme.KateListening
+import com.dti.kate.ui.theme.KateSpeaking
+import com.dti.kate.ui.theme.Purple70
+import com.dti.kate.ui.theme.Error
 // app/src/main/java/com/dti/kate/ui/components/KateAvatar.kt
 
 @Composable
