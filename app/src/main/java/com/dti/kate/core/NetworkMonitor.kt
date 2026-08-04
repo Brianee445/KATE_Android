b80b1a901a76fb68942b818f3f7ddef3db117dd1
Brianee445 <edewesley13@gmail.com>
2026-08-04 01:28:38 +0100

    Create NetworkMonitor.kt

---
+++ app/src/main/java/com/dti/kate/core/NetworkMonitor.kt
@@ -0,0 +1,16 @@
+package com.dti.kate.core
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkMonitor {
+    fun isOnline(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+            ?: return false
+        val network = cm.activeNetwork ?: return false
+        val capabilities = cm.getNetworkCapabilities(network) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    }
+}
